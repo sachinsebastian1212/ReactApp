3159e23fb0f4c0ae5102da93e27fb29e94b8e4dd
sachinsebastian12 <sachinsebastian12@hotmail.com>
2019-04-08 23:11:02 +0530

    impemented form creation using json

--- src/test_data.js
+++ src/test_data.js
@@ -1,4 +1,57 @@
-{[
+var data1 = [{
+    "type": "questset",
+    "id": "qs_1",
+    "children": [{
+        "type": "quest",
+        "id": "q_1",
+        "children": [{
+            "type": "objectivelist",
+            "id": "ol_1",
+            "children": [{
+                "type": "objective",
+                "id": "o_1"
+            }]
+        }]
+    }]
+}];
+
+var data2 = [
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_1"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_1"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_1"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_1"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+];
+var gdata2 ;
+/**
+ * two objectives
+ */
+var data3 =[
     {
         "type": "questset",
         "id": "qs_1",
@@ -25,4 +78,657 @@
             }
         ]
     }
-]
+];
+
+/**
+ * two objective list
+ */
+var data4 = [
+    {
+        "type": "questset",
+        "id": "qs_1",
+        "children": [
+            {
+                "type": "quest",
+                "id": "q_1",
+                "children": [
+                    {
+                        "type": "objectivelist",
+                        "id": "ol_1",
+                        "children": [
+                            {
+                                "type": "objective",
+                                "id": "o_1"
+                            },
+                            {
+                                "type": "objective",
+                                "id": "o_2"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "objectivelist",
+                        "id": "ol_2",
+                        "children": [
+                            {
+                                "type": "objective",
+                                "id": "o_3"
+                            },
+                            {
+                                "type": "objective",
+                                "id": "o_4"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+];
+var gdata4 ;
+//Two quest set
+var data5 = [
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_1"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_1"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_1"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_1"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_2"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_2"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_2"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_2"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+];
+
+/**
+ * two quests
+ */
+var data6 = [
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_1"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_1"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_1"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_1"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_2"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_2"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_2"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+];
+
+
+var data7 = [
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_1"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_1"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_1"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_1"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_2"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_2"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_2"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_2"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_2"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_2"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_2"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_2"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_2"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_2"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_2"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_2"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_2"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_2"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_2"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_2"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_2"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_2"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_2"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_2"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_2"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_2"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_2"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_2"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_2"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_2"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_2"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_2"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_2"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_2"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_2"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_2"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_2"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_2"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_2"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_2"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_2"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_2"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_2"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_2"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_2"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_2"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_2"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_2"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_2"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_2"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_2"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_2"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_2"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_2"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_2"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_2"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_2"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_2"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_2"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_2"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "data": {
+            "type": "questset",
+            "id": "qs_2"
+        },
+        "children": [
+            {
+                "data": {
+                    "type": "quest",
+                    "id": "q_2"
+                },
+                "children": [
+                    {
+                        "data": {
+                            "type": "objectivelist",
+                            "id": "ol_2"
+                        },
+                        "children": [
+                            {
+                                "data": {
+                                    "type": "objective",
+                                    "id": "o_2"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+
+];
--- src/traversal.jsx
+++ src/traversal.jsx
@@ -3,7 +3,13 @@ import data from './test.json';
 
 var objectivesRetrieved = [{ "desc": "desc1", "val": "ob_77" }, { "desc": "desc2", "val": "ob_78" },
 { "desc": "desc3", "val": "ob_79" }, { "desc": "desc4", "val": "ob_80" }];
+var rewardsRetrieved = [{ "desc": "reward1", "val": "qr_26" }, { "desc": "reward2", "val": "qr_31" },
+{ "desc": "reward3", "val": "qr_32" }, { "desc": "reward4", "val": "qr_33" }];
+var questActionTypeRetrived = ["Type1", "Type2", "Type3"];
+var questActionRetrived = ["action1", "action2", "action3"];
 
+
+var select_options = ["auto", "predefined"];
 
 class QuestForm extends Component {
 
@@ -39,10 +45,11 @@ class Node extends Component {
 
     return (
       <li>
-        <span>{this.props.node.type}</span>
-        <ol data-nodetype={this.props.node.type}>
+        <ol data-nodetype={this.props.node.type} className="test">
           <NodeElements data={this.props.node} />
-          {childnodes}
+          {childnodes ?
+            <li><ol>{childnodes}</ol></li>
+            : null}
         </ol>
       </li>
     );
@@ -61,26 +68,66 @@ class NodeElements extends Component {
     switch (this.props.data.type) {
       case "objective":
         return (
-          <li>
-            <label htmlFor="">Select objective</label>
-            <select>
-            {objectivesRetrieved.map((obj) => <option value={obj.val}>{obj.desc}</option>)}
-            </select>
-          </li>
+          <NodeElement>
+            <li>
+              <label htmlFor="">Select objective</label>
+              <select>
+                {objectivesRetrieved.map((obj) => <option value={obj.val}>{obj.desc}</option>)}
+              </select>
+            </li>
+          </NodeElement>
         );
-
-        break;
       case "objectivelist":
-
-        break;
-
+        return (
+          <NodeElement>
+            <li>
+              <label htmlFor="">ObjectiveList creation</label>
+              <select>
+                {select_options.map((elem) => <option value={elem}>{elem}</option>)}
+              </select>
+            </li>
+          </NodeElement>
+        );
       case "quest":
-
-        break;
+        return (
+          <NodeElement>
+            <li>
+              <label htmlFor="">Quest creation</label>
+              <select>{select_options.map((elem) => <option value={elem}>{elem}</option>)}</select>
+            </li>
+            <li><label htmlFor="">Title</label><input data-type="title" type="text" name="title" id="" /></li>
+            <li><label htmlFor="">Description</label><input data-type="description" type="text" name="description" id="" /></li>
+            <li>
+              <label htmlFor="">Reward</label>
+              <select data-type="reward">{rewardsRetrieved.map((elem) => <option value={elem.val}>{elem.desc}</option>)}</select>
+            </li>
+            <li>
+              <label htmlFor="">QuestActionType</label>
+              <select data-type="questActionType">{questActionTypeRetrived.map((elem) => <option value={elem}>{elem}</option>)}</select>
+            </li>
+            <li>
+              <label htmlFor="">QuestAction</label>
+              <select data-type="questAction" >{questActionRetrived.map((elem) => <option value={elem}>{elem}</option>)}</select>
+            </li>
+          </NodeElement>
+        );
 
       case "questset":
 
-        break;
+        return (
+          <NodeElement>
+            <li>
+              <label htmlFor="">Quest creation</label>
+              <select>{select_options.map((elem) => <option value={elem}>{elem}</option>)}</select>
+            </li>
+            <li>
+              <label htmlFor="">Reward</label>
+              <select data-type="rewardId">{rewardsRetrieved.map((elem) => <option value={elem.val}>{elem.desc}</option>)}</select>
+            </li>
+            <li><label htmlFor="">Difficulty</label><input data-type="difficulty" type="text" name="" id=""/></li>
+            
+          </NodeElement>
+        );
 
       default:
         break;
@@ -92,4 +139,10 @@ class NodeElements extends Component {
       </ol>
     );
   }
+}
+
+function NodeElement(props) {
+  return (
+    props.children
+  );
 }
